0ef2303ff52b1c3ab0f974105ecffba173b49d0b
Danillo Neves Souza <danillo.neves97@gmail.com>
2023-05-26 22:50:04 -0300

    Implemented Player Jumping and Moving with Platforms

--- Assets/Scripts/Handlers/PlayerHandler.cs
+++ Assets/Scripts/Handlers/PlayerHandler.cs
@@ -7,19 +7,28 @@ namespace Handlers
     {
         [SerializeField] private float moveSpeed = 5f;
         [SerializeField] private bool useXMovement = true;
-        [SerializeField] private bool useYMovement = false;
         [SerializeField] private bool useZMovement = true;
         
-        [Header("Camera Vertical Settings")]
-        [SerializeField] private float minXAngle = -20f;
-        [SerializeField] private float maxXAngle = 10f;
+        [Header("Jumping")]
+        [SerializeField] private float jumpForce = 5f;
+        [SerializeField] private bool useJump = true;
         
+        [Header("Ground Check")]
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private LayerMask platformLayer;
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private float groundCheckRadius = 0.1f;
+
         private Transform playerTransform;
+        private Rigidbody playerRigidbody;
+        private bool isGrounded;
+        private bool lastPlatformDetected;
 
         private void Start()
         {
             playerTransform = transform;
-            
+            playerRigidbody = GetComponent<Rigidbody>();
+
             // Lock the mouse in the center and hide it
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
@@ -27,51 +36,74 @@ namespace Handlers
         
         private void Update()
         {
+            CheckForGround();
+            CheckForPlatform();
+            Jump();
             Move();
             LookAround();
+        }
+
+        private void CheckForGround()
+        {
+            var groundCheckPosition = groundCheck.position;
+            isGrounded = Physics.CheckSphere(groundCheckPosition, groundCheckRadius, groundLayer);
+        }
+        
+        private void CheckForPlatform()
+        {
+            if (!isGrounded)
+            {
+                return;
+            }
+            
+            var platformDetected = Physics.CheckSphere(groundCheck.position, groundCheckRadius, platformLayer);
+
+            if (platformDetected)
+            {
+                var platform = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, platformLayer)[0].transform;
+                playerTransform.parent = platform;
+            }
+            else
+            {
+                playerTransform.parent = null;
+            }
+            
+            lastPlatformDetected = platformDetected;
+        }
+
+        private void Jump()
+        {
+            if (!Input.GetKeyDown(KeyCode.Space) || !useJump || !isGrounded)
+            {
+                return;
+            }
+            
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
 
         private void Move()
         {
             var x = Input.GetAxis("Horizontal");
-            var y = Input.GetAxis("Jump");
             var z = Input.GetAxis("Vertical");
             
             var movement = new Vector3 
             ( 
                 useXMovement ? x : 0f, 
-                useYMovement ? y : 0f,
+                0f,
                 useZMovement ? z : 0f
             );
       
             playerTransform.position += transform.forward * (movement.z * moveSpeed * Time.deltaTime);
             playerTransform.position += transform.right * (movement.x * moveSpeed * Time.deltaTime);
-            playerTransform.position += transform.up * (movement.y * moveSpeed * Time.deltaTime);
-            
-            // Player Position should be clamped to the ground
-            playerTransform.position = new Vector3(playerTransform.position.x, 0f, playerTransform.position.z);
         }
         
         private void LookAround()
         {
             // Use the mouse to look around
             var mouseX = Input.GetAxis("Mouse X");
-            var mouseY = Input.GetAxis("Mouse Y");
-            
+
             // Rotate the player around the Y axis
             playerTransform.Rotate(Vector3.up * (mouseX * 100f * Time.deltaTime));
-            
-            // Rotate the player around the X axis
-            playerTransform.Rotate(Vector3.left * (mouseY * 100f * Time.deltaTime));
-            
-            // Clamp the X rotation using minXAngle and maxXAngle
-            var xRotation = playerTransform.eulerAngles.x;
-            if (xRotation > 180f)
-            {
-                xRotation -= 360f;
-            }
-            xRotation = Mathf.Clamp(xRotation, minXAngle, maxXAngle);
-            playerTransform.eulerAngles = new Vector3(xRotation, playerTransform.eulerAngles.y, 0f);
         }
     }
 }

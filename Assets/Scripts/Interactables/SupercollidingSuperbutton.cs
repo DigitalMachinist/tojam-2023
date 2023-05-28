730b263d9165f1857afd1f7b084e70219ce7385b
Jeff Rose <jrose0@gmail.com>
2023-05-28 15:17:58 -0400

    Bugfix for floor button

--- Assets/Scripts/Interactables/SupercollidingSuperbutton.cs
+++ Assets/Scripts/Interactables/SupercollidingSuperbutton.cs
@@ -5,23 +5,25 @@ namespace Interactables
     [RequireComponent(typeof(Collider))]
     public class SupercollidingSuperbutton : Switch
     {
+        private int numActivations = 0;
+        
         void OnTriggerEnter(Collider other)
         {
-            if (!other.CompareTag("Box") && !other.CompareTag("Player"))
+            if (!other.CompareTag("Box"))
             {
                 return;
             }
-            
+
             Activate();
         }
         
         void OnTriggerExit(Collider other)
         {
-            if (!other.CompareTag("Box") && !other.CompareTag("Player"))
+            if (!other.CompareTag("Box"))
             {
                 return;
             }
-            
+
             Deactivate();
         }
     }
